3f815354504748bc8bb4d8f1e11b863166f6a68e
jordan-fokoua <jordan.fokoua@datadome.co>
2024-03-08 21:24:13 +0100

    Add support for Tag switching (XHR/JS Tag)

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,23 @@
 const LS_RO_KEY = 'dd-request-options';
 
 function addMainEventListeners() {
-  document
-    .getElementById('submit-request')
-    .addEventListener('click', function () {
-      makeApiCall();
-      saveRequestOptions();
-    });
+  document.getElementById('submit-request').addEventListener('click', function () {
+    makeApiCall();
+    saveRequestOptions();
+  });
 }
 
 function saveRequestOptions() {
-  const requestType = document.querySelector(
-    'input[name="requestType"]:checked'
-  ).value;
-  const largeResponseBody =
-    document.getElementById('largeResponseBody').checked;
-  const responseType = document.querySelector(
-    'input[name="responseType"]:checked'
-  ).value;
+  const requestType = document.querySelector('input[name="requestType"]:checked').value;
+  const largeResponseBody = document.getElementById('largeResponseBody').checked;
+  const abortSignal = document.getElementById('abortSignal').checked;
+  const responseType = document.querySelector('input[name="responseType"]:checked').value;
 
   const requestOptions = {
     requestType,
     largeResponseBody,
     responseType,
+    abortSignal,
   };
 
   localStorage.setItem(LS_RO_KEY, JSON.stringify(requestOptions));
@@ -33,34 +28,36 @@ function updateRequestOptionsOnLoad() {
 
   if (requestOptions) {
     if (requestOptions.requestType) {
-      const id = `use${
-        requestOptions.requestType.charAt(0).toUpperCase() +
-        requestOptions.requestType.slice(1)
-      }`;
+      const id = `use${requestOptions.requestType.charAt(0).toUpperCase() + requestOptions.requestType.slice(1)}`;
       document.getElementById(id).checked = true;
     }
 
     if (requestOptions.largeResponseBody !== undefined) {
-      document.getElementById('largeResponseBody').checked =
-        requestOptions.largeResponseBody;
+      document.getElementById('largeResponseBody').checked = requestOptions.largeResponseBody;
+    }
+
+    if (requestOptions.abortSignal !== undefined) {
+      document.getElementById('abortSignal').checked = requestOptions.abortSignal;
     }
 
     if (requestOptions.responseType) {
-      document.getElementById(
-        `${requestOptions.responseType}Response`
-      ).checked = true;
+      document.getElementById(`${requestOptions.responseType}Response`).checked = true;
     }
   }
 }
 
+function getSelectedRadioValue(name) {
+  const selectedRadioButton = document.querySelector(`input[name="${name}"]:checked`);
+  return selectedRadioButton ? selectedRadioButton.value : null;
+}
+
 function makeApiCall() {
   const useFetch = document.getElementById('useFetch').checked;
-  const isLargeResponseBody =
-    document.getElementById('largeResponseBody').checked;
-  const isBlocked = document.getElementById('blockResponse').checked;
-  const action = isBlocked ? 'block' : 'allow';
+  const isLargeResponseBody = document.getElementById('largeResponseBody').checked;
+  const abortSignal = document.getElementById('abortSignal').checked;
+  const action = getSelectedRadioValue('responseType');
 
-  const args = { action, isLargeResponseBody };
+  const args = { action, isLargeResponseBody, abortSignal };
   if (useFetch) {
     sendFetch(args);
   } else {
@@ -68,15 +65,23 @@ function makeApiCall() {
   }
 }
 
+const controller = new AbortController();
+const signal = controller.signal;
+
 function sendFetch(params) {
-  const { action } = params;
-  fetch(document.location.origin + '/api/' + action, {
+  const { action, abortSignal } = params;
+  const url = document.location.origin + '/api/' + action;
+  const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(params),
-  })
+    signal: abortSignal ? AbortSignal.timeout(1) : null,
+  };
+  // const fetchPromise = true ? fetch(new Request(url), options) : fetch(url, options);
+
+  fetch(url, options)
     .then((response) => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
